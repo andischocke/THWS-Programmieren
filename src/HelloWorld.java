8179966a0467be59ada113af9b4c9542f23db973
Andreas <Andreas.Henneberger@outlook.com>
2024-03-28 14:56:38 +0100

    Create HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        // Übungsblatt 1 Aufgabe 2:
+        System.out.println("Hello World!");
+
+        // Übungsblatt 1 Aufgabe 3:
+        int solution = 3 * (9 + 3) + 4 * 8;
+        System.out.println("3 * (9 + 3) + 4 * 8 = " + solution);
+    }
+}
